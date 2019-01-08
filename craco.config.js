35ec5fedca78d8fe1535f86e9f6ccd5412a1069e
aaronchenwei <aaronchenwei@users.noreply.github.com>
2019-01-08 10:22:03 +0800

    close #17

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,26 @@
+const path = require('path');
+const util = require('util');
 const CracoAntDesignPlugin = require('craco-antd');
+
+const inspect = obj => util.inspect(obj, { showHidden: true, depth: null });
+
+const findPlugin = (plugins, pluginName) =>
+  plugins.find(plugin => plugin.constructor.name === pluginName);
 
 module.exports = {
   babel: {
     plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]],
+  },
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      const forkTsCheckerWebpackPlugin = findPlugin(
+        webpackConfig.plugins,
+        'ForkTsCheckerWebpackPlugin'
+      );
+      forkTsCheckerWebpackPlugin.tslint = path.resolve('tslint.json');
+
+      return webpackConfig;
+    },
   },
   plugins: [{ plugin: CracoAntDesignPlugin }],
 };
